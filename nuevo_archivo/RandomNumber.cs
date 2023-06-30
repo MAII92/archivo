3631e5d364f08856c9c7d1e46b04a24daba55d54
Maydy Elena Aguilar Mosquera <maguilarm@SUMMIT.COM.CO>
2023-06-30 16:23:15 -0500

    ARCHIVO

--- nuevo_archivo/RandomNumber.cs
+++ nuevo_archivo/RandomNumber.cs
@@ -12,12 +12,24 @@ public class RandomNumber
         return numeroAleatorio;
 
     }
+    private static bool SumatoriaMayorACero(long numero)
+    {
+        long sumatoria = 0;
+
+        while (numero > 0)
+        {
+            sumatoria += numero % 10;
+            numero /= 10;
+        }
+
+        return sumatoria > 0;
+    }
     public static string GenerateNewCode(Int32 codeLength)
     {
         Random random = new Random();
         StringBuilder output = new StringBuilder();
 
-        for (Int32 i = 0; i < codeLength; i++)
+        for (Decimal i = 0; i < codeLength; i++)
         {
             output.Append(random.Next(0, 10));
         }
@@ -25,15 +37,16 @@ public class RandomNumber
         return output.ToString();
     }
 
-   public  Decimal GenerarDecimal()
+   public static Decimal GenerarDecimal()
     {
         Random rnd = new Random();
         Int32 valorEntero = rnd.Next(1000000, 10000000);
         Decimal valorDecimal = rnd.Next(0, 10000) / 100m;
         Decimal val = new Decimal(valorEntero) + valorDecimal;
         return val;
-    }
+   }
 }
+
 
 
  
